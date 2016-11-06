bc4f29c73b2845e9b150a1b8579e844d29a2fca3
Bill McKessy <bill@evident.io>
2016-11-06 07:44:09 -0800

    good job bill

--- index.js
+++ index.js
@@ -37,10 +37,12 @@ Perceptron.prototype.constructor = Perceptron;
 var memory_width = 2048
 var image_size = 256 * 240 * 3
 
+var memory_size_to_load = 32
+
 console.log('making the network')
-// var myNetwork = new Architect.Perceptron(memory_width, 2, image_size)
+var myNetwork = new Architect.Perceptron(memory_size_to_load + 1, 8, 32, 1)
 console.log('done making the network')
-// var trainer = new Trainer(myNetwork)
+var trainer = new Trainer(myNetwork)
 
 var memory_data = JSON.parse(fs.readFileSync('./memory_data.json').toString())
 var image_data = []
@@ -50,10 +52,15 @@ console.log('mem data length', memory_data.length)
 var jpegData
 var rawImageData
 console.log('loading image data')
-memory_data.slice(0,1).forEach(function(v,idx){
-  if(idx%10 === 0){
-    console.log(idx)
+var images_to_load = 2
+var mem_step = Math.ceil(memory_data.length / images_to_load)
+
+memory_data.forEach(function(v,idx){
+
+  if(idx % mem_step !== 0){
+    return
   }
+  console.log(idx)
   jpegData = fs.readFileSync('./output/'+idx+'.jpg')
   rawImageData = jpeg.decode(jpegData, true)
   var img = []
@@ -70,14 +77,56 @@ memory_data.slice(0,1).forEach(function(v,idx){
   image_data.push(img)
 })
 
-console.log('image_data length', image_data.length)
-console.log(image_data[0].length)
+console.log('number of images loaded', image_data.length)
+console.log('values found in the image', image_data[0].length)
 
-// var trainingSet = [
-//   {
-//     input: [0,0],
-//     output: [0]
-//   },
-// ]
+// build the training data for each pixel
+// input: [[memory],[pixel_index]]    // memory + pixel index
+// output: pixel_bufer[pixel_index]   // pixel value
 
-// trainer.train(trainingSet);
+var trainingSet = []
+image_data.forEach(function(d,idx){
+
+  var memory_data_remapped = memory_data[idx].slice(0,memory_size_to_load)
+  memory_data_remapped = memory_data_remapped.map(function(o){return o *= (1.0/255.0) })
+
+  // for each pixel
+  d.forEach(function(pixel_value, pixel_idx){
+    // append the pixel index to the memory data
+    var input_data = memory_data_remapped.concat([pixel_idx/d.length])
+    // console.log(input_data[memory_size_to_load])
+    trainingSet.push({
+      input: input_data,
+      output: [pixel_value].map(function(o){return o *= (1.0/255.0) })
+    })
+  })
+  console.log(idx,'training set size', trainingSet.length)
+})
+
+// trainer.train(trainingSet.slice(0,1000),{
+console.log('runing trainer')
+trainer.train(trainingSet,{
+    rate: .001,
+    iterations: 1000,
+    error: .005,
+    shuffle: false,
+    log: 1,
+    cost: Trainer.cost.CROSS_ENTROPY,
+    schedule: {
+      every: 20, // repeat this task every 500 iterations
+      do: function(data) {
+          console.log('writing network')
+          fs.writeFileSync('./network.json', JSON.stringify(myNetwork.toJSON(),null,2), 'utf-8')
+          // custom log
+          // console.log("error", data.error, "iterations", data.iterations, "rate", data.rate);
+          // if (someCondition)
+              // return true; // abort/stop training
+          return false;
+      }
+    }
+});
+
+fs.writeFileSync('./network.json', JSON.stringify(myNetwork.toJSON(),null,2), 'utf-8')
+
+// trainer.train(trainingSet.slice(0,100));
+console.log('dunzo')
